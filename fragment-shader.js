2f855abb1245928000388c21d8f65d1b334d4249
Serendipitous19 <aahan1729@gmail.com>
2025-01-20 17:39:48 +0530

    added pan and zoom

--- fragment-shader.js
+++ fragment-shader.js
@@ -9,6 +9,13 @@ uniform vec2 u_resolution;
 uniform vec2 u_translate;
 uniform vec2 u_scale;
 
+vec4 getColor(int i, int maxIterations) {
+    // Normalize the iteration count to a value between 0 and 1
+    float t = sin(3.14159265358979323 * float(i) / float(maxIterations));
+    return vec4(t, t, t, 1.0);
+}
+
+
 void main() {
     // gl_FragColor is a special variable a fragment shader is responsible for setting
     vec2 pixelCoords = (v_coords.xy * u_resolution * 0.5);
@@ -16,11 +23,12 @@ void main() {
 
     vec2 z = vec2(0.0, 0.0);
     vec4 finalColor = vec4(0.0, 0.0, 0.0, 1.0);
-    for(int i = 0; i < 1000; i++){
+    const int max_iterations = 500;
+    for(int i = 0; i < max_iterations; i++){
         z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y); // square it
         z += t; // add c
         if(z.x * z.x + z.y * z.y > 4.0) {
-            finalColor = vec4(0.0, 0.0, float(i) / 100.0, 1.0);
+            finalColor = getColor(i, max_iterations);
             break;
         }
     }
